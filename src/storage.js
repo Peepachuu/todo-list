efa4f06c0832d81249e933d53761000c0d6080b0
Zafir <zafirhasan2006@gmail.com>
2022-11-05 07:55:39 +0400

    Create functionality for storing todos and projects in localStorage

--- src/storage.js
+++ src/storage.js
@@ -13,15 +13,16 @@ export function insertTodoInStorage(todoToInsert) {
     const position = projectsStorage.findIndex(project => project.title == projectTitle);
     
     projectsStorage[position].todos.push(todoToInsert);
-    todoToInsert.parentProject = projectsStorage[position];
+    todoToInsert.parentProject = projectsStorage[position].title;
 }
 
 export function deleteTodoFromStorage(todoToDelete) {
-    const position = projectsStorage.findIndex(project => project == todoToDelete.parentProject);
+    const position = projectsStorage.findIndex(project => project.title == todoToDelete.parentProject);
     projectsStorage[position].todos.splice(projectsStorage[position].todos.findIndex(todo => todo == todoToDelete), 1);
 }
 
 export function findImportantTodos() {
+    showStoredData();
     let tasks = [];
     projectsStorage.forEach(project => {
         project.todos.forEach(todo => {
@@ -38,7 +39,6 @@ export function findTodosForToday() {
         project.todos.forEach(todo => {
             let format = todo.dueDate.split("-");
             format = format.map(x => Number(x));
-            console.log(format, isToday(new Date(2022, 10, 1)));
             if (isToday(new Date(format[0], format[1] - 1, format[2])))
                 tasks.push(todo);
         })
@@ -57,4 +57,35 @@ export function findTodoForThisWeek() {
         })
     })
     return tasks;
+}
+
+function saveDataInLocalStorage() {
+    localStorage.setItem("projectsStorage", JSON.stringify(projectsStorage));
+    console.log(localStorage.getItem("projectsStorage"));
+}
+
+function storageAvailable(type) {
+    let storage;
+    try {
+        storage = window[type];
+        const x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch (e) {
+        return e instanceof DOMException && (
+            e.code === 22 ||
+            e.code === 1014 ||
+            e.name === 'QuotaExceededError' ||
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            (storage && storage.length !== 0);
+    }
+}
+
+function showStoredData() {
+    if (!storageAvailable("localStorage"))
+        return ;
+    projectsStorage = JSON.parse(localStorage.getItem("projectsStorage"));
+    console.log(projectsStorage);
 }
